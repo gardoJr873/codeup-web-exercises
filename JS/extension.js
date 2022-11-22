c9cdb2ba4a09abf2560ca6776db614b06e1da57f
edgardojr <edgardo.ortiz.jr11@gmail.com>
2022-11-22 09:51:24 -0600

    Created JS extension within index

---
+++ JS/extension.js
@@ -0,0 +1,13 @@
+/*console.log(alert('Hello'));*/
+
+"use strict";
+
+/*let age = 25;
+console.log(age);*/
+
+/*let message = 'Hey Guy';
+alert(message);*/
+
+let age = prompt(message: 'What is your age?');
+
+alert('DocRob, you are ${age} old = Wow!');
